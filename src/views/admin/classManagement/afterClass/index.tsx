cc9e37106087a7196e7941f451a0f4cf6b1f728d
manhquang00m <vumanhquang36@gmail.com>
2023-10-03 01:06:24 +0700

    add header ui after class

--- src/views/admin/classManagement/afterClass/index.tsx
+++ src/views/admin/classManagement/afterClass/index.tsx
@@ -7,6 +7,10 @@ import {
   StackDivider,
   Text,
   Stack,
+  Image,
+  Flex,
+  Spacer,
+  Progress,
 } from "@chakra-ui/react";
 // Custom components
 import Banner from "views/admin/profile/components/Banner";
@@ -31,15 +35,17 @@ import {
 } from "@chakra-ui/react";
 import { yupResolver } from "@hookform/resolvers/yup";
 import * as yup from "yup";
-import MiniCalendar from "components/calendar/MiniCalendar";
+import kienThucImg from 'assets/img/classManage/kienthuc.png';
+import baiHocImg from 'assets/img/classManage/baihoctiep.png';
+
 
 export default function AfterClass() {
   const schema = yup.object({
-    name: yup.string().required("heloo"),
+    teacher: yup.string().required("Đây là trường hợp bắt buộc !"),
   });
   const { handleSubmit, control } = useForm({
     defaultValues: {
-      name: "",
+      teacher: "",
     },
     resolver: yupResolver(schema),
   });
@@ -57,12 +63,12 @@ export default function AfterClass() {
             <form onSubmit={handleSubmit(onSubmit)}>
               <Controller
                 control={control}
-                name="name"
+                name="teacher"
                 render={({ field: { ref, ...restField }, fieldState }) => {
                   return (
                     <FormControl isInvalid={!!fieldState?.error}>
-                      <FormLabel htmlFor="name">Name</FormLabel>
-                      <Input {...restField} id="name" placeholder="name" />
+                      <FormLabel htmlFor="teacher">Tên giáo viên</FormLabel>
+                      <Input {...restField} placeholder="Nhập tên ..." />
                       <FormErrorMessage>
                         {fieldState?.error?.message}
                       </FormErrorMessage>
@@ -70,16 +76,63 @@ export default function AfterClass() {
                   );
                 }}
               />
-              <MiniCalendar h="100%" minW="100%" selectRange={false} />
-
               <Button mt={4} colorScheme="teal" type="submit">
                 Submit
               </Button>
             </form>
           </Card>
         </GridItem>
-        <GridItem w="100%" bg="blue.500">
-          sdsdsdsdsd
+        <GridItem w="100%" >
+          <Card>
+            {/* heading */}
+            <Flex alignItems='center' h={'50px'}>
+              <Image h='40px' w='175px' src='https://cdn0257.cdn4s.com/media/logo-02%20(1)-01.png' alt='logo' />
+              <Spacer />
+              <Heading>After Class</Heading>
+              <Spacer />
+              <Box>
+                <Text>Date: 02/12/2023</Text>
+                <Text>Teacher: Vu Manh Quang</Text>
+              </Box>
+            </Flex>
+            <Grid mt={4} templateColumns='repeat(5, 1fr)' gap={4}>
+              <GridItem colSpan={4} w='100%' >
+                <Grid templateColumns='repeat(2, 1fr)' gap={4}>
+                  <GridItem w='100%' >
+                    <Flex minHeight={'100%'} alignItems={'center'} bg='#f4f4f4' p={4} borderRadius={'xl'}>
+                      <Image h='60px' w='60px' src={kienThucImg} alt='logo' />
+                      <Box ml={4}>
+                        <Heading as='h4' size='md'>
+                          Kiến Thức Đạt Được
+                        </Heading>
+                        <Text>Khái niệm về Class, lập trình hướng đối tượng,lập trình hướng đối tượng</Text>
+                      </Box>
+                    </Flex>
+                  </GridItem>
+                  <GridItem w='100%' >
+                    <Flex minHeight={'100%'} alignItems={'center'} bg='#f4f4f4' p={4} borderRadius={'xl'}>
+                      <Image h='60px' w='60px' src={baiHocImg} alt='logo' />
+                      <Box ml={4}>
+                        <Heading as='h4' size='md'>
+                          Bài Học Tiếp Theo
+                        </Heading>
+                        <Text>Khái niệm về Class</Text>
+                      </Box>
+                    </Flex>
+                  </GridItem>
+                </Grid>
+
+                {/* Table */}
+
+              </GridItem>
+              <GridItem w='100%' className="flex items-center">
+                <Progress w='100%' height='48px' value={60} className="-rotate-90" />
+              </GridItem>
+            </Grid>
+            <Box>
+
+            </Box>
+          </Card>
         </GridItem>
       </Grid>
     </Box>
