46247af3dbf55251873268d6a50a793ac9653147
manhquang00m <vumanhquang36@gmail.com>
2023-10-05 00:17:28 +0700

    form array comments

--- src/views/admin/classManagement/afterClass/index.tsx
+++ src/views/admin/classManagement/afterClass/index.tsx
@@ -12,20 +12,23 @@ import {
   Table,
   Thead,
   Tbody,
-  Tfoot,
   Tr,
   Th,
   Td,
   TableCaption,
   TableContainer,
   Center,
+  SimpleGrid,
+  Stack,
+  StackDivider,
+  Divider,
 } from "@chakra-ui/react";
 
 // Assets
 import banner from "assets/img/auth/banner.png";
 import avatar from "assets/img/avatars/avatar4.png";
 import Card from "components/card/Card";
-import { useForm, Controller } from "react-hook-form";
+import { useForm, Controller, useFieldArray } from "react-hook-form";
 import html2canvas from "html2canvas";
 
 import {
@@ -43,18 +46,34 @@ import logoITS from "assets/img/layout/logoITS.png";
 import { toPng } from 'html-to-image';
 
 
-import { useRef } from "react";
-import { MdPhone, MdPublic } from "react-icons/md";
+import { useEffect, useRef } from "react";
+import { MdDeleteOutline, MdOutlineNoteAdd, MdPhone, MdPublic } from "react-icons/md";
 export default function AfterClass() {
   const schema = yup.object({
     teacher: yup.string().required("Đây là trường hợp bắt buộc !"),
+    date: yup.string(),
+    comments: yup.array()
   });
   const { handleSubmit, control } = useForm({
     defaultValues: {
-      teacher: "",
+      teacher: undefined,
+      date: undefined,
     },
     resolver: yupResolver(schema),
   });
+
+  const { fields, append, remove } = useFieldArray({
+    control,
+    name: "comments"
+  });
+
+  useEffect(() => {
+    append({
+      student: undefined,
+      comment: undefined,
+      result: undefined
+    })
+  }, [])
 
   const onSubmit = async (values: any) => {
     console.log("Form: ", values);
@@ -78,25 +97,123 @@ export default function AfterClass() {
   return (
     <Box pt={{ base: "130px", md: "80px", xl: "80px" }}>
       {/* Main Fields */}
-      <Grid templateColumns="repeat(2, 1fr)" gap={4}>
-        <GridItem colSpan={{ base: 2, "2xl": 1 }} w="100%">
+      <Grid w="100%" templateColumns="repeat(2, 1fr)" gap={4}>
+        <GridItem colSpan={{ base: 2, "2xl": 2 }} w="100%">
           <Card>
             <form onSubmit={handleSubmit(onSubmit)}>
-              <Controller
-                control={control}
-                name="teacher"
-                render={({ field: { ref, ...restField }, fieldState }) => {
-                  return (
-                    <FormControl isInvalid={!!fieldState?.error}>
-                      <FormLabel htmlFor="teacher">Tên giáo viên</FormLabel>
-                      <Input {...restField} placeholder="Nhập tên ..." />
-                      <FormErrorMessage>
-                        {fieldState?.error?.message}
-                      </FormErrorMessage>
-                    </FormControl>
-                  );
-                }}
-              />
+              <SimpleGrid columns={{ base: 1, "2xl": 2 }} spacing={4}>
+                <Card variant="elevated" className="p-3 border">
+                  <Heading size='md' mb={3}>Thông tin chung</Heading>
+                  <SimpleGrid columns={{ base: 1, xl: 2 }} spacing={2}>
+                    <Controller
+                      control={control}
+                      name="teacher"
+                      render={({ field: { ref, ...restField }, fieldState }) => {
+                        return (
+                          <FormControl isInvalid={!!fieldState?.error}>
+                            <FormLabel >Tên giáo viên</FormLabel>
+                            <Input {...restField} placeholder="Nhập tên ..." />
+                            <FormErrorMessage>
+                              {fieldState?.error?.message}
+                            </FormErrorMessage>
+                          </FormControl>
+                        );
+                      }}
+                    />
+                    <Controller
+                      control={control}
+                      name="date"
+                      render={({ field: { ref, ...restField }, fieldState }) => {
+                        return (
+                          <FormControl isInvalid={!!fieldState?.error}>
+                            <FormLabel>Ngày dạy</FormLabel>
+                            <Input type={'date'} {...restField} placeholder="Chọn ngày ..." />
+                            <FormErrorMessage>
+                              {fieldState?.error?.message}
+                            </FormErrorMessage>
+                          </FormControl>
+                        );
+                      }}
+                    />
+                  </SimpleGrid>
+                </Card>
+
+                {/* Nhan xet */}
+                <Card variant="elevated" className="p-3 border">
+                  <Heading size='md' mb={3}>Nhận xét học sinh</Heading>
+                  {/* reder list comments */}
+                  {fields.map((item, index) => {
+                    return <Grid key={item.id} className="bg-[#f8f8f8] p-2 rounded-md mb-4" templateColumns='repeat(24, 1fr)' gap={2}>
+                      <GridItem colSpan={{ base: 24, xl: 7 }}>
+                        <Controller
+                          control={control}
+                          name={`comments.${index}.student`}
+                          render={({ field: { ref, ...restField }, fieldState }) => {
+                            return (
+                              <FormControl isInvalid={!!fieldState?.error}>
+                                <FormLabel >Học sinh {`(${index + 1})`}</FormLabel>
+                                <Input backgroundColor={"white"} {...restField} placeholder="Nhập tên học sinh ..." />
+                                <FormErrorMessage>
+                                  {fieldState?.error?.message}
+                                </FormErrorMessage>
+                              </FormControl>
+                            );
+                          }}
+                        />
+                      </GridItem>
+                      <GridItem colSpan={{ base: 24, xl: 7 }}>
+                        <Controller
+                          control={control}
+                          name="teacher"
+                          render={({ field: { ref, ...restField }, fieldState }) => {
+                            return (
+                              <FormControl isInvalid={!!fieldState?.error}>
+                                <FormLabel htmlFor="teacher">Nhận xét buổi học</FormLabel>
+                                <Input backgroundColor={"white"} {...restField} placeholder="Nhập nhận xét ..." />
+                                <FormErrorMessage>
+                                  {fieldState?.error?.message}
+                                </FormErrorMessage>
+                              </FormControl>
+                            );
+                          }}
+                        />
+                      </GridItem>
+                      <GridItem colSpan={{ base: 24, xl: 7 }}>
+                        <Controller
+                          control={control}
+                          name="teacher"
+                          render={({ field: { ref, ...restField }, fieldState }) => {
+                            return (
+                              <FormControl isInvalid={!!fieldState?.error}>
+                                <FormLabel htmlFor="teacher">Kết quả học tập</FormLabel>
+                                <Input backgroundColor={"white"} {...restField} placeholder="Nhập tên ..." />
+                                <FormErrorMessage>
+                                  {fieldState?.error?.message}
+                                </FormErrorMessage>
+                              </FormControl>
+                            );
+                          }}
+                        />
+                      </GridItem>
+                      <GridItem className="flex items-end justify-center" colSpan={{ base: 24, xl: 3 }}>
+                        <Button className="bg-white border border-red-500 text-red-500 hover:bg-red-500 hover:text-white" w={"100%"} onClick={() => remove(index)} rightIcon={<MdDeleteOutline />}>
+                          Xóa
+                        </Button>
+                      </GridItem>
+                    </Grid>
+                  })}
+
+                  <Button variant="outline" rightIcon={<MdOutlineNoteAdd />} w={{ base: "100%", xl: "160px" }} mt={2} colorScheme="blue" onClick={() => {
+                    append({
+                      student: undefined,
+                      comment: undefined,
+                      result: undefined
+                    })
+                  }}>
+                    Thêm học sinh
+                  </Button>
+                </Card>
+              </SimpleGrid>
               <Button mt={4} colorScheme="teal" type="submit">
                 Submit
               </Button>
@@ -113,8 +230,8 @@ export default function AfterClass() {
         </GridItem>
 
         {/* img after class */}
-        <GridItem ref={afrerClassRef} colSpan={{ base: 2, "2xl": 1 }} w="100%">
-          <Card>
+        <GridItem overflow={{ base: 'scroll', xl: "hidden" }} colSpan={{ base: 2, "2xl": 2 }} >
+          <Card ref={afrerClassRef} minWidth={{ base: "1024px", "2xl": "100%" }}>
             {/* heading */}
             <Flex alignItems="center" h={"50px"}>
               <Image h="40px" w="130px" src={logoITS} alt="logo" />
