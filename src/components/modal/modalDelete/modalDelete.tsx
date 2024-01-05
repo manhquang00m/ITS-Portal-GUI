8c123a006849103e27d7f6930fff653f0a9c8304
manhquang00m <vumanhquang36@gmail.com>
2024-01-06 02:26:41 +0700

    fix modal width

--- src/components/modal/modalDelete/modalDelete.tsx
+++ src/components/modal/modalDelete/modalDelete.tsx
@@ -26,14 +26,14 @@ export default function DeleteModal(props: IDeleteModalProps) {
   return (
     <Modal isOpen={isOpen} onClose={onClose}>
       <ModalOverlay p={4} />
-      <ModalContent p={4}>
+      <ModalContent p={{ base: 0, md: 2, xl: 4 }} width={{ base: '90%', lg: "inherit" }}>
         <ModalHeader paddingBottom={0}>Xác nhận xoá </ModalHeader>
         <ModalCloseButton />
         <ModalBody>
           Bạn có đồng ý xoá dữ liệu có id là{" "}
           <span className="font-bold">{id}</span> không ?
         </ModalBody>
-        <ModalFooter px={0}>
+        <ModalFooter px={{ base: 5, md: 2, lg: 0 }}>
           <Button onClick={handleEnroll} variant="brand">
             Xác nhận
           </Button>
